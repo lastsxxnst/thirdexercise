0f9add226da24bd972e9b30f8fd9d6f012533a06
lastsxxnst <167818521+lastsxxnst@users.noreply.github.com>
2024-06-28 01:05:35 +0300

    Add files via upload

---
+++ thirdexercise.py
@@ -0,0 +1,16 @@
+﻿import rsa
+
+# Генерация ключей
+(public_key, private_key) = rsa.newkeys(2048)
+
+# Ваше сообщение
+plaintext = input("Enter a plaintext: ")
+
+# Шифрование
+ciphertext = rsa.encrypt(plaintext.encode(), public_key)
+
+# Дешифрование
+decrypted_text = rsa.decrypt(ciphertext, private_key).decode()
+
+print("Encrypted text:", ciphertext)
+print("Decrypted text:", decrypted_text)
